babd8c53dd837bda7b0ab13bd655f3bce8fe2be3
Gulyás Dávid <72173665289@szily.lan>
2022-03-29 10:23:02 +0200

    Gui fájl megnyitása és tartalom kiolvasása

--- UresGuiGit/Form1.cs
+++ UresGuiGit/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,8 +20,36 @@ namespace UresGuiGit
 
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenFileDialog op = new OpenFileDialog();
-            op.ShowDialog();
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            if (fileContent.Length != 0)
+            {
+                fileContent += fileContent.Length;
+                MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
